06d5cc2519be04488674f9602a4e460b5289c38f
BianCai <kpmoving@gmail.com>
2018-12-27 17:52:47 +0800

    update

--- docs/genIndex.sh
+++ docs/genIndex.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"; pwd
 
-echo "<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>分明子博客文章汇总 </title></head><body> <h2>分明子博客文章汇总</h2> <p>共 $(find . -size +1500c -name "Post_*" |wc -l) 篇文章，最后更新时间：<em>$(date "+%Y-%m-%d %H:%M:%S" )</em></p>  <ol>" >index.html
+echo "<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>分明子博客文章汇总 </title></head><body> <h2>分明子博客文章汇总</h2> <p>共 $(find . -size +1500c -name "Post_*" |wc -l) 篇文章，最后更新时间：<em>$(date "+%Y-%m-%d %H:%M:%S" )</em></p>  <a href=\"index2.html\">灵 界 见 闻</a> <ol>" >index.html
 for i in $(find . -size +1500c -name "Post_*" |sort -r)  
 do  
 title=$(head $i| grep "<h2>"|cut -d\> -f2 |cut -d\< -f1)
@@ -12,4 +12,7 @@ done
 
 echo "</ol></body></html>"  >>index.html
 
+echo "<html> <head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" /><title>分明子博客文章汇总 </title></head><body> <h2>分明子灵界见闻</h2> <p>共 $(grep 灵界 index.html|wc -l) 篇文章，最后更新时间：<em>$(date "+%Y-%m-%d %H:%M:%S" )</em></p>  <ol>" >index2.html
+grep 灵界 index.html >> index2.html
 
+echo "</ol></body></html>"  >>index2.html
